2c462916053ad89d679e29558b45845814924d21
mzajbe <mzajbe@gmail.com>
2026-07-29 01:51:21 +0600

    anagram

---
+++ js/problems/anagram.js
@@ -0,0 +1,8 @@
+//anagram
+/**
+ * 1. the length of the two string are same.
+ * 2. there are same characters available .
+ * 3. the frequency of each character are same.
+ * 4. just the order or character can be different.
+ * for example (listen,silent),(race,care)
+ */
